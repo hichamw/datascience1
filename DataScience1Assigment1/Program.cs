b9c1a5d9ffa7da3a14f60f0ed4945b8fcd8179b9
Hicham Wakrim <0897587@hr.nl>
2018-04-12 17:12:36 +0200

    Formatting

--- DataScience1Assigment1/Program.cs
+++ DataScience1Assigment1/Program.cs
@@ -18,54 +18,60 @@ namespace DataScience1Assigment1
         static void Main(string[] args)
         {
             string userItemName = "userItem.data";
-            string parentFolder = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName;
-            string userItemPath = Path.Combine(parentFolder, userItemName);
-            string outputFile = Path.Combine(parentFolder, "write.txt");
-            Array text = File.ReadAllLines(userItemPath);
-
-            Dictionary<string, Dictionary<string, string>> userPreferences =
-                new Dictionary<string, Dictionary<string, string>>();
-
-            foreach (string line in text)
+            var directoryInfo = Directory.GetParent(Directory.GetCurrentDirectory()).Parent;
+            if (directoryInfo != null)
             {
-                var splitLine = line.Split(',');
-                string user = splitLine[0];
-                string item = splitLine[1];
-                string rating = splitLine[2];
+                string parentFolder = directoryInfo.FullName;
+                string userItemPath = Path.Combine(parentFolder, userItemName);
+                string outputFile = Path.Combine(parentFolder, "userRatings.txt");
+                Array text = File.ReadAllLines(userItemPath);
 
-                if (userPreferences.ContainsKey(user))
+                Dictionary<string, Dictionary<string, string>> userPreferences =
+                    new Dictionary<string, Dictionary<string, string>>();
+
+                foreach (string line in text)
                 {
-                    if (!userPreferences[user].ContainsKey(item))
+                    var splitLine = line.Split(',');
+                    string user = splitLine[0];
+                    string item = splitLine[1];
+                    string rating = splitLine[2];
+
+                    if (userPreferences.ContainsKey(user))
                     {
+                        if (!userPreferences[user].ContainsKey(item))
+                        {
+                            userPreferences[user].Add(item, rating);
+                        }
+                    }
+                    else
+                    {
+                        userPreferences.Add(user, new Dictionary<string, string>());
                         userPreferences[user].Add(item, rating);
                     }
                 }
-                else
-                {
-                    userPreferences.Add(user, new Dictionary<string, string>());
-                    userPreferences[user].Add(item, rating);
-                }
-            }
-            
-            Console.WriteLine(CalculateSimilarity("pearson", userPreferences["3"], userPreferences["4"]));
 
-            foreach (var user in userPreferences)
-            {
-                using (StreamWriter writetext = new StreamWriter(outputFile, true))
-                {
-                    writetext.WriteLine("User {0} gave these ratings:", user.Key);
-                    foreach (var rating in user.Value)
-                    {
-                        writetext.WriteLine("{0}:{1}", rating.Key, rating.Value);
-                    }
-                }
+//                //Following code writes the userPrefences dictionary into a file called userPreferences.txt
+//                foreach (var user in userPreferences)
+//                {
+//                    using (StreamWriter writetext = new StreamWriter(outputFile, true))
+//                    {
+//                        writetext.WriteLine("User {0} gave these ratings:", user.Key);
+//                        foreach (var rating in user.Value)
+//                        {
+//                            writetext.WriteLine("{0}:{1}", rating.Key, rating.Value);
+//                        }
+//                    }
+//                }
+                
+                Console.WriteLine(CalculateSimilarity(userPreferences["1"], userPreferences["2"]));
             }
         }
 
-        private static double CalculateSimilarity(string strategy, Dictionary<string, string> user1,
-            Dictionary<string, string> user2)
+        private static double CalculateSimilarity(Dictionary<string, string> user1, Dictionary<string, string> user2)
         {
             double answer = 0;
+            Console.WriteLine("What calculation strategy do you want to use? Please type in euclidean, pearson or cosine.");
+            string strategy = Console.ReadLine();
             switch (strategy)
             {
                 case "euclidean":
@@ -84,8 +90,6 @@ namespace DataScience1Assigment1
 
         private static double EuclideanSimilarity(Dictionary<string, string> user1, Dictionary<string, string> user2)
         {
-            double answer = 0.0;
-
             var ratingsSum = 0.0;
             foreach (var rating in user1)
             {
@@ -98,7 +102,7 @@ namespace DataScience1Assigment1
 
             var distance = Math.Sqrt(ratingsSum);
 
-            answer = 1 / (distance + 1);
+            double answer = 1 / (distance + 1);
 
             return answer;
         }
@@ -130,7 +134,7 @@ namespace DataScience1Assigment1
 
             double bottomLeft = Math.Sqrt(sumPowUser1 - Math.Pow(sumUser1, 2.0) / count);
             double bottomRight = Math.Sqrt(sumPowUser2 - Math.Pow(sumUser2, 2.0) / count);
-            
+
             var answer = (topLeft - topRight) / (bottomLeft * bottomRight);
 
             return answer;
@@ -148,18 +152,19 @@ namespace DataScience1Assigment1
                 {
                     double rating1 = Convert.ToDouble(rating.Value);
                     double rating2 = Convert.ToDouble(user2[rating.Key]);
-                    
+
                     top += (rating1 * rating2);
                     bottomLeft += Math.Pow(rating1, 2.0);
                     bottomRight += Math.Pow(rating2, 2.0);
                 }
             }
+
             bottomLeft = Math.Sqrt(bottomLeft);
             bottomRight = Math.Sqrt(bottomRight);
             double bottom = bottomLeft * bottomRight;
 
             double answer = top / bottom;
-            
+
             return answer;
         }
     }
